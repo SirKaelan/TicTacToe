d3481c50a7431f34fb6bca5316f1c83b0bb8d9f2
SirKaelan <kaelanfreakerman6@gmail.com>
2023-04-20 15:03:07 +0300

    Import required state from context

--- src/components/GameOverMessage.js
+++ src/components/GameOverMessage.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useIsGameActive, useGameWinner } from "./BoardContext";
 
 const GameOverMessage = () => {
+  const isGameActive = useIsGameActive();
+  const gameWinner = useGameWinner();
+
+  // Write logic for message decision
   return <p>Message component!</p>;
 };
 
